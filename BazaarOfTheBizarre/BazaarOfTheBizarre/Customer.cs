ff301a98dc312d48a9b18bab7b67e676b3686c52
Hesselberg, Marius <hesmar15@egms.no>
2017-11-14 15:14:59 +0100

    Fjernet de autogenererte meldingene

--- BazaarOfTheBizarre/BazaarOfTheBizarre/Customer.cs
+++ BazaarOfTheBizarre/BazaarOfTheBizarre/Customer.cs
@@ -1,12 +1,4 @@
-﻿/*
- * Created by SharpDevelop.
- * User: Håvard
- * Date: 14.11.2017
- * Time: 14.56
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
-using System;
+﻿using System;
 
 namespace BazaarOfTheBizarre
 {
