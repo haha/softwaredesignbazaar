79174ab548ec705db841b9851ef04b8593e7a6db
Håvard Halvorsen <h.halvorsen@live.no>
2017-11-14 15:01:35 +0100

    Added first files

---
+++ BazaarOfTheBizarre/BazaarOfTheBizarre/Store.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Håvard
+ * Date: 14.11.2017
+ * Time: 14.51
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace BazaarOfTheBizarre
+{
+	/// <summary>
+	/// Description of Store.
+	/// </summary>
+	public class Store
+	{
+		public Store()
+		{
+		}
+	}
+}
